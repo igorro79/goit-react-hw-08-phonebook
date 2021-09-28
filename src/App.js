1b03eb643e7433e1bb4ed2c83a481c90466ca205
Igor Griniov <griniov.igor@gmail.com>
2021-09-29 01:28:48 +0300

    project base settings done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+  render() {
+    return <></>;
+  }
+}
+
+export default App;
